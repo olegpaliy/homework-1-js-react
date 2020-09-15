69e7cc4c75c40c62b01518aa32342bf71da2c386
Oleh <goleh@ukr.net>
2020-09-15 16:50:01 +0300

    1,2 tasks

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// завдання 1
+let yearsUser = prompt("Скільки тобі років?")
+alert(`Тобі ${yearsUser} років`);
+
+//завдання 2
+const this_year = 2020;
+let yearOFborn = +prompt('коли ти народився?')
+let born = this_year - yearOFborn ;
+alert(`Привіт, мені ${born} років`);
